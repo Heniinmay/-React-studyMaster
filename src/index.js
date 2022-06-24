17ff7dbdc3e59bfa518ca04086b27dd8f62c36c0
IDB_heni@idealbloom.io <yjshim>
2022-06-24 15:29:48 +0900

    styled-component: theme > darkmode 만들기

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
 import ReactDOM from "react-dom";
 
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+const lightTheme = {
+  textColor: "111",
+  backgroundColor: "#whitesmoke",
+};
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
