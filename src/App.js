0541a15eef1cf6a503078e421b55c79dba2d3b10
IDB_heni@idealbloom.io <yjshim>
2022-06-24 13:33:31 +0900

    styled-component: attrs 예제

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Btn>Login</Btn>
-      <Btn as="a" href="/">
-        Log in
-      </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
