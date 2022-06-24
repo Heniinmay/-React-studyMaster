030201ac0c70f7511d6c4a2f32ae1427e6033dce
IDB_heni@idealbloom.io <yjshim>
2022-06-24 14:15:48 +0900

    styled-component: animation 적용 예제

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
-const Input = styled.input.attrs({ required: true })`
+const RotationAnimation = keyframes`
+0%{
+ transform:rotate(0deg);
+ border-radius:0px;
+}50%{
+  transform:rotate(360deg);
+  border-radius:100px;
+}100%{
+  transform:rotate(0deg);
+  border-radius:0px;
+}
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  animation: ${RotationAnimation} 2s linear infinite;
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
