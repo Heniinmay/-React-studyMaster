d6ced62ce46ef7c35edd6a9d24f8f952e9dffc7d
IDB_heni@idealbloom.io <yjshim>
2022-07-19 13:29:56 +0900

    3.3 Optional props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,19 +3,29 @@ import styled from "styled-components";
 //코스 실행전에 오류들을 확인 할수 있다. 코드에서,
 interface ContainerProps {
   bgColor: string;
+  borderColor?: string;
 }
 const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+  // 적용 값 ?? Default 값
 }
 
 export default Circle;
