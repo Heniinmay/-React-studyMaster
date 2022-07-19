cb75938bd0c238000028fde7ddcaf1893aa8d59b
IDB_heni@idealbloom.io <yjshim>
2022-07-19 14:41:00 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    //현재,  TS는 이 onChange 함수가 InputElement에 의해서 실행 될 것이라 안다.
+    // console.log(e.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </>
   );
 }
